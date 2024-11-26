6c8745cd3a4fbb24691763835932abb1b16209e6
François <francois.dewez@student.hepl.be>
2024-11-26 13:04:55 +0100

    Init

---
+++ Step1/LibSerHV.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "data.h"
+
+void AProposServeurHV(char *Version,char *Nom1,char* Nom2) ;
+
+int rechercheHV(char*NomFichier, int Reference, struct VehiculeHV *UnRecord);
